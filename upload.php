02d9d6234eb9d5ddd4147e54d67f83ab5c8359ba
Edier Heraldo Hernandez Molano <ehhernandez81@misena.edu.co>
2021-10-19 21:28:56 -0500

    Implementación básica de Tesseract para leer los textos presentes en una imagen

---
+++ upload.php
@@ -0,0 +1,31 @@
+<?php
+if (isset($_FILES['image'])) {
+    // $imagick = new Imagick();
+
+
+
+    $file_name = $_FILES['image']['name'];
+    $file_tmp = $_FILES['image']['tmp_name'];
+    move_uploaded_file($file_tmp, "images/" . $file_name);
+    echo "<h3>Image Upload Success</h3>";
+    echo '<img src="images/' . $file_name . '" style="width:100%">';
+
+    shell_exec('"C:\\Program Files\\Tesseract-OCR\\tesseract" "C:\\xampp\\htdocs\\image2text\\images\\' . $file_name . '" out');
+
+    echo "<br><h3>OCR after reading</h3><br><pre>";
+
+    $myfile = fopen("out.txt", "r") or die("Unable to open file!");
+    $information = fread($myfile, filesize("out.txt"));
+    $keyWords = ['tarjeta', 'identidad', 'identificación', 'documento'];
+    $arrInfo = explode(' ', $information);
+    foreach ($arrInfo as $info) {
+        foreach ($keyWords as $words) {
+            if ($info == $words) {
+                echo "Se ha encontrado una concordancia, las palabras: " . $info;
+            }
+        }
+    }
+    print_r($arrInfo);
+    fclose($myfile);
+    echo "</pre>";
+}
